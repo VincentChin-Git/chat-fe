1b7403688745b2888767d7ab9a5f807bc1a21bdd
VincentChin-Git <124128510+VincentChin-Git@users.noreply.github.com>
2023-11-15 22:25:33 +0800

    chore: add contact page

--- src/screens/contact/Contact.tsx
+++ src/screens/contact/Contact.tsx
@@ -1,9 +1,10 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import Footer from '../../components/common/Footer';
 import Header from '../../components/common/Header';
 import StatusHeader from '../../components/common/StatusHeader';
+import ProfileImage from '../../components/contact/ProfileImage';
 import { commonStyles } from '../../constants/styles';
 import { IContactPopulate } from '../../types/contact';
 import { ITheme } from '../../types/theme';
@@ -32,6 +33,23 @@ const Contact = ({ navigation, route }: { navigation: any; route: any }) => {
         title={contactInfo.nickname || ''}
         showBack
       />
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            margin: 15,
+            rowGap: 10,
+          }}>
+          <ProfileImage dim={100} avatar={contactInfo.avatar} />
+          <Text style={{ fontWeight: '700', fontSize: 18 }}>
+            +65 {contactInfo.mobile?.slice(0, 4)} {contactInfo.mobile?.slice(4)}
+          </Text>
+
+          <Text style={{ fontSize: 16 }}>{contactInfo.describe}</Text>
+        </View>
+      </View>
       <Footer navigation={navigation} selected={0} />
     </View>
   );
